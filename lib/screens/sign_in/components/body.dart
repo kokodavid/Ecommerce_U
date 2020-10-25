40e32279dfc56735729d3dc85e02790522d5991b
kingkgdave@gmail.com <betcydave18>
2020-10-25 13:44:01 +0300

    Email form validation

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce/components/custom_surfix_icon.dart';
+import 'package:ecommerce/components/default_button.dart';
+import 'package:ecommerce/components/form_error.dart';
 import 'package:ecommerce/constants.dart';
 import 'package:ecommerce/size_config.dart';
 import 'package:flutter/material.dart';
@@ -39,28 +41,92 @@ class SignForm extends StatefulWidget {
 }
 
 class _SignFormState extends State<SignForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> errors = [];
   @override
   Widget build(BuildContext context) {
     return Form(
+    key: _formKey,
       child: Column(
         children: [
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            decoration:
-            InputDecoration(
-              labelText: "Email",
-              hintText: "Input your Email",
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              suffixIcon: CustomSuffixIcon(
-                svgIcon: "assets/icons/Mail.svg",
-              ),
-            ),
+          buildEmailFormField(),
+          SizedBox(height: getProportionateScreenHeight(20),
+          ),
+          buildPasswordFormField(),
+          SizedBox(height: getProportionateScreenHeight(20)),
+          FormError(errors: errors),
+          DefaultButton(
+            text: "Continue",
+            press: () {
+              if(_formKey.currentState.validate()){
+                _formKey.currentState.save();
+              }
+            },
           )
         ],
       ),
     );
   }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: "Password",
+            hintText: "Input your Password",
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            suffixIcon: CustomSuffixIcon(
+              svgIcon: "assets/icons/Lock.svg",
+            ),
+          ),
+        );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+          onChanged: (value) {
+            if (value.isNotEmpty && errors.contains(kEmailNullError)){
+              setState(() {
+                errors.remove(kEmailNullError);
+              });
+            }else if(emailValidatorRegExp.hasMatch(value) &&
+                errors.contains(kEmailNullError)){
+              setState(() {
+                errors.remove(kEmailNullError);
+              });
+
+            }
+            return null;
+          },
+          validator: (value){
+            if (value.isEmpty && !errors.contains(kEmailNullError)){
+              setState(() {
+                errors.add(kEmailNullError);
+              });
+            }else if(emailValidatorRegExp.hasMatch(value) &&
+                !errors.contains(kEmailNullError)){
+              setState(() {
+                errors.add(kEmailNullError);
+
+              });
+            }
+            return null;
+          },
+          decoration:
+          InputDecoration(
+            labelText: "Email",
+            hintText: "Input your Email",
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            suffixIcon: CustomSuffixIcon(
+              svgIcon: "assets/icons/Mail.svg",
+            ),
+          ),
+        );
+  }
 }
+
+
 
 
 

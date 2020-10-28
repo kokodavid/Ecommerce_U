409bf7ad073d07e050c88cce3cb78e7226092116
David <kingkgdave@gmail.com>
2020-10-28 08:57:51 +0300

    Sign up Forms validations

--- lib/screens/sign_up/components/body.dart
+++ lib/screens/sign_up/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce/components/custom_surfix_icon.dart';
+import 'package:ecommerce/components/default_button.dart';
+import 'package:ecommerce/components/form_error.dart';
 import 'package:ecommerce/constants.dart';
 import 'package:ecommerce/size_config.dart';
 import 'package:flutter/material.dart';
@@ -38,6 +40,8 @@ class _SignUpFormState extends State<SignUpForm> {
   String password;
   String confirm_password;
   final List<String> errors = [];
+  final _formKey = GlobalKey<FormState>();
+
 
   void addError({String error}) {
     if(!errors.contains(error))
@@ -55,11 +59,22 @@ class _SignUpFormState extends State<SignUpForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
           buildEmailFormField(),
+          SizedBox(height: getProportionateScreenHeight(30),),
           buildPassFormField(),
-          buildPassFormField()
+          SizedBox(height: getProportionateScreenHeight(30),),
+          buildConfirmPassFormField(),
+          SizedBox(height: getProportionateScreenHeight(30),),
+          FormError(errors: errors),
+          DefaultButton(text: "Continue",
+            press: () {
+              if(_formKey.currentState.validate()){
+                //Go to complete profile
+              }
+            },)
         ],
       ),
     );
@@ -97,6 +112,40 @@ class _SignUpFormState extends State<SignUpForm> {
           ),
         );
   }
+
+  TextFormField buildConfirmPassFormField() {
+    return TextFormField(
+      obscureText: true,
+      onChanged: (value) {
+        if (value.isNotEmpty ){
+          removeError(error: kPassNullError );
+        }else if(value.length >= 8){
+          removeError(error: kPassNullError);
+        }
+        return null;
+
+      },
+      validator: (value){
+        if (value.isEmpty ){
+          addError(error: kPassNullError );
+          return "";
+        }else if(value.length < 8){
+          addError(error: kShortNullError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Confirm Password",
+        hintText: "Confirm your Password",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSuffixIcon(
+          svgIcon: "assets/icons/Lock.svg",
+        ),
+      ),
+    );
+  }
+
 
   TextFormField buildEmailFormField() {
     return TextFormField(

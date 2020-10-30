8fa88af5bd4c03f2232e64824e43693ec64ec7ee
bill <billodida420@gmail.com>
2020-10-30 15:14:10 +0300

    complete profile introduction text

--- lib/screens/complete_profile/components/body.dart
+++ lib/screens/complete_profile/components/body.dart
@@ -1,5 +1,8 @@
+import 'package:ecommerce/components/custom_surfix_icon.dart';
 import 'package:ecommerce/size_config.dart';
 import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -36,11 +39,109 @@ class CompleteProfileForm extends StatefulWidget {
 }
 
 class _CompleteProfileFormState extends State<CompleteProfileForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> errors = [];
+  String firstName;
+  String lastName;
+  String phoneNumber;
+  String address;
+
+  void addError({String error}){
+    if(!errors.contains(errors))
+      setState(() {
+        errors.add(error);
+      });
+  }
+
+  void removeError({String error}){
+    if(errors.contains(error)){
+      setState(() {
+        errors.remove(error);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
+      child: Padding(
+        padding:  EdgeInsets.symmetric(horizontal:getProportionateScreenWidth(20)),
+        child: Column(
+          children: [
+            buildFnameFormField(),
+            SizedBox(height: getProportionateScreenHeight(20),),
+            buildLastNameFormField()
+          ],
+        ),
+      ),
+
 
     );
+  }
+
+  TextFormField buildLastNameFormField() {
+    return TextFormField(
+            onSaved:  (newValue) => lastName = newValue,
+            onChanged: (value) {
+              if (value.isNotEmpty){
+                removeError(error: kEmailNullError);
+              }else if(emailValidatorRegExp.hasMatch(value)){
+                removeError(error: kInvalidEmailError);
+              }
+              return null;
+            },
+            validator: (value){
+              if (value.isEmpty){
+                addError(error: kEmailNullError);
+              }else if(!emailValidatorRegExp.hasMatch(value)){
+                addError(error: kInvalidEmailError);
+                return "";
+              }
+              return null;
+            },
+            decoration:
+            InputDecoration(
+              labelText: "Last Name",
+              hintText: "Input your Last Name",
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              suffixIcon: CustomSuffixIcon(
+                svgIcon: "assets/icons/User.svg",
+              ),
+            ),
+          );
+  }
+
+  TextFormField buildFnameFormField() {
+    return TextFormField(
+            onSaved:  (newValue) => firstName = newValue,
+            onChanged: (value) {
+              if (value.isNotEmpty){
+                removeError(error: kEmailNullError);
+              }else if(emailValidatorRegExp.hasMatch(value)){
+                removeError(error: kInvalidEmailError);
+              }
+              return null;
+            },
+            validator: (value){
+              if (value.isEmpty){
+                addError(error: kEmailNullError);
+              }else if(!emailValidatorRegExp.hasMatch(value)){
+                addError(error: kInvalidEmailError);
+                return "";
+              }
+              return null;
+            },
+            decoration:
+            InputDecoration(
+              labelText: "First name",
+              hintText: "Input your First Name",
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              suffixIcon: CustomSuffixIcon(
+                svgIcon: "assets/icons/User.svg",
+              ),
+            ),
+          );
   }
 }
 

a5b5fcca6a3830317066a49db87e0e23c6fae799
bill <billodida420@gmail.com>
2020-11-03 15:58:57 +0300

    OTP verification Introduction

--- lib/screens/Otp_Verification/components/Body.dart
+++ lib/screens/Otp_Verification/components/Body.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/constants.dart';
 import 'package:flutter/material.dart';
 
 import '../../../size_config.dart';
@@ -9,16 +10,26 @@ class Body extends StatelessWidget {
       width: double.infinity,
       child: Column(
         children: [
-          Text("OTP Verification",
-          style: TextStyle(
-          fontSize: getProportionateScreenHeight(28),
-            fontWeight: FontWeight.bold,
-            color: Colors.black
-    ),
-          ),
           Text(
-            "We Sent your code to 0769042076"
-          )
+            "OTP Verification",
+            style: TextStyle(
+                fontSize: getProportionateScreenHeight(28),
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
+          ),
+          Text("We Sent your code to 0769042076"),
+          Row(children: [
+            Text("This Code will expire in",
+            textAlign: TextAlign.center,
+            ),
+
+            TweenAnimationBuilder(
+                tween: Tween(begin: 30.0, end: 0),
+                duration: Duration(seconds: 30),
+                builder: (context, value, child) => Text("00:${value.toInt()}", style: TextStyle(
+                  color: kPrimaryColor
+                ),))
+          ])
         ],
       ),
     );

3588f1fe2bd052a205afb145e3386a0391538f86
kokodavid <kingkgdave@gmail.com>
2020-11-09 14:56:52 +0300

    Otp input text form fields

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:ecommerce/screens/home/components/body.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  static String routeName = "/home";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
